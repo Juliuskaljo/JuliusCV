672e1670576451d3425c3bed9ef3554efeb0d0d1
Julius Kaljo <julius.kaljo@hotmail.se>
2024-04-15 18:55:58 +0200

    rewritten about me, and fixed hamburgermenu

--- julius-cv/src/Components/About.jsx
+++ julius-cv/src/Components/About.jsx
@@ -16,9 +16,27 @@ function About() {
 	<div  className="about-text">
 	  <h1 >Om mig</h1>
 	  <p>
-		Jag är en frontend-utvecklare som brinner för att skapa användarvänliga och snygga webbplatser. Jag har en kandidatexamen i webbutveckling och har arbetat med webbutveckling i två år. Jag har erfarenhet av att arbeta med HTML, CSS, JavaScript och React. Jag är en kreativ och noggrann person som alltid strävar efter att skapa en bra användarupplevelse.
-		Jag är en frontend-utvecklare som brinner för att skapa användarvänliga och snygga webbplatser. Jag har en kandidatexamen i webbutveckling och har arbetat med webbutveckling i två år. Jag har erfarenhet av att arbeta med HTML, CSS, JavaScript och React. Jag är en kreativ och noggrann person som alltid strävar efter att skapa en bra användarupplevelse.Jag är en frontend-utvecklare som brinner för att skapa användarvänliga och snygga webbplatser. Jag har en kandidatexamen i webbutveckling och har arbetat med webbutveckling i två år. Jag har erfarenhet av att arbeta med HTML, CSS, JavaScript och React. Jag är en kreativ och noggrann person som alltid strävar efter att skapa en bra användarupplevelse.
-	  </p>
+	  	Hej,
+      	Mitt namn är Julius Kaljo. Jag är 24 år och bor i centrala Göteborg
+		tillsammans med min flickvän.</p><p> 
+		Jag är en glad prick som tycker om att samarbeta med människor. Jag
+ 		skulle (ödmjukt) beskriva mig själv som social, lyhörd och har väldigt
+		lätt till skratt.</p> <p> Fritiden spenderar jag helst med vänner och familj. Några
+		favoritintressen är dataspel, golf och fotboll. </p><p>
+		Just nu läser jag Front-End Developer på NBI Handelsakademin. Det
+ 		som tilltalar mig med kodning och UX-design är de olika stegen i
+ 		skapandet. Planeringen, utförandet och resultatet. Att utifrån en
+ 		beställning se produkten sakta men säkert växa fram i realtid. Jag har
+ 		under det här året utvecklats enormt och märker redan nu att det här
+ 		är något man aldrig kommer sluta utvecklas inom. </p>
+		<p>
+		Under perioden 11 november 2024 till 31 januari 2025 och 17 mars 2025 till 6 juni 2025, söker jag praktik inom Front-End-utveckling. Jag är mycket intresserad av att få möjligheten att arbeta inom området och ser fram emot att bidra med mina kunskaper och lära mig under praktikperioden. </p>
+		<p>
+		Ser fram emot att höra från potentiella arbetsgivare och hoppas på att få möjlighet att göra praktik hos er!</p>
+		<p>
+		Allt gott så länge,
+		Julius Kaljo
+		</p>
 	</div>
 	
   </section>
--- julius-cv/src/Components/Header.jsx
+++ julius-cv/src/Components/Header.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./header.css";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  return (
-    <nav  className={`navbar ${isOpen ? 'open' : ''}`}>
-      <button className={`hamburger-btn ${isOpen ? 'open' : ''}`} onClick={toggleMenu} aria-label="Toggle Menu">
-        <span className="hamburger-icon"></span>
-      </button>
+  const handleClickOutside = (event) => {
+	if (menuRef.current && !menuRef.current.contains(event.target)) {
+	  setIsOpen(false);
+	}
+  };
 
-      <div className={`menu-content ${isOpen ? 'open' : ''}`}>
-        <ul>
-          <li><a href="#top" className="link">Hem</a></li>
-          <li><a href="#about" className="link">Om mig</a></li>
-          <li><a href="#projects" className="link">Mina projekt</a></li>
-          <li><a href="#knowledge" className="link">Kunskaper</a></li>
-          <li><a href="#contact" className="link">Kontakt</a></li>
-        </ul>
-		
-      </div>
-    </nav>
-  );
+  useEffect(() => {
+	document.addEventListener("mousedown", handleClickOutside);
+	return () => {
+	  document.removeEventListener("mousedown", handleClickOutside);
+	}
+	  }, []);
+
+
+  return (
+	<nav className={`navbar ${isOpen ? 'open' : ''}`}>
+	<button className={`hamburger-btn ${isOpen ? 'open' : ''}`} onClick={toggleMenu} aria-label="Toggle Menu">
+	  <span className="hamburger-icon"></span>
+	</button>
+
+	<div ref={menuRef} className={`menu-content ${isOpen ? 'open' : ''}`}>
+	  <ul>
+		<li><a href="#top" className="link" onClick={() => setIsOpen(false)}>Hem</a></li>
+		<li><a href="#about" className="link" onClick={() => setIsOpen(false)}>Om mig</a></li>
+		<li><a href="#projects" className="link" onClick={() => setIsOpen(false)}>Mina projekt</a></li>
+		<li><a href="#knowledge" className="link" onClick={() => setIsOpen(false)}>Kunskaper</a></li>
+		<li><a href="#contact" className="link" onClick={() => setIsOpen(false)}>Kontakt</a></li>
+	  </ul>
+	</div>
+  </nav>
+);
 }
 
 export default Header;
